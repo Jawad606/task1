01dbf1814c105dc44f02a9c68f488477a476bc2b
darksoul <jawadmirza606@gmail.com>
2023-01-10 22:57:38 +0500

    first commit  df

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css'
 function App() {
   return (
     <div className="App">
+      {/* Import login Page */}
       <Login/>
     </div>
   );
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import { Button, Form, Input } from "antd";
 import "./Login.css";
 import { Typography } from "antd";
+// React hooks
 import { useRef,useState  } from 'react';
 
 const Login = () => {
+  // Ant Design components
   const { Title } = Typography;
   const errRef = useRef();
   const [errMsg, setErrMsg] = useState('');
   const onFinish = (values) => {
     console.log("Success:", values);
+    // This condition will validate the email and password
     if (
     (values.email === "test_user@meistery.net" ||
     values.email === "test_user2@meistery.net") &&
@@ -17,16 +20,18 @@ const Login = () => {
   ){
     setErrMsg(''); 
   } else {
+    // adding the error message 
     setErrMsg('Either your password or login is incorrect');
   }
   errRef.current.focus();
   };
-
+  // showing the errors if the email is not filled
   const onFinishFailed = (errorInfo) => {
     console.log("Failed:", errorInfo);
   };
   return (
     <div className="login">
+      {/* Ant Design form */}
       <Form
         name="basic"
         labelCol={{ span: 24 }}
@@ -56,6 +61,7 @@ const Login = () => {
         >
           <Input.Password className="custom__input" placeholder="Password" />
         </Form.Item>
+        {/* Tag to show error message */}
         <p ref={errRef} style={{color:'red',padding:0,margin: 0}} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
         <Form.Item wrapperCol={{ span: 24 }}>
           <Button className="custom__input"  type="primary" htmlType="submit">
